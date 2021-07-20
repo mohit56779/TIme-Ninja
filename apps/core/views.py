fc9a268fe31cf02c6bbbae7b6752391480fab13c
Mohit <mohit56779@gmail.com>
2021-07-20 11:33:59 -0600

    added privact, plans etc. basic pages

--- apps/core/views.py
+++ apps/core/views.py
@@ -6,3 +6,9 @@ from django.shortcuts import render
 # Views
 def frontpage(request):
     return render(request,'core/frontpage.html')
+
+def privacy(request):
+    return render(request,'core/privacy.html')
+
+def plans(request):
+    return render(request,'core/plans.html')
--- timeNinja/urls.py
+++ timeNinja/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from apps.core.views import frontpage
+from apps.core.views import frontpage, privacy, plans
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('privacy/', privacy, name='privacy'),
+    path('plans/', plans, name='plans'),
     path('admin/', admin.site.urls),
 ]
