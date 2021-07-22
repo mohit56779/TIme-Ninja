81095fcb83783937ed8d92148ef129344c7e9bff
Mohit <mohit56779@gmail.com>
2021-07-21 21:27:22 -0600

    added editing entry and deleting entry functionalities

--- apps/project/views.py
+++ apps/project/views.py
@@ -97,3 +97,46 @@ def edit_task(request, project_id, task_id):
             return redirect('project:task', project_id=project.id, task_id=task.id)
 
     return render(request, 'project/edit_task.html', {'project': project, 'task': task}) 
+
+@login_required
+def edit_entry(request, project_id, task_id, entry_id):
+    project = get_object_or_404(Project, pk=project_id)
+    task = get_object_or_404(Task, pk=task_id)
+    entry = get_object_or_404(Entry, pk=entry_id)
+
+    if request.method == 'POST':
+        hours = int(request.POST.get('hours', 0))
+        minutes = int(request.POST.get('minutes', 0))
+
+        date = '%s %s' % (request.POST.get('date'), datetime.now().time())
+
+        entry.created_at = date
+        entry.minutes = (hours * 60) + minutes
+        entry.save()
+
+        messages.info(request, 'The changes was saved!')
+
+        return redirect('project:task', project_id=project.id, task_id=task.id)
+
+    hours, minutes = divmod(entry.minutes, 60)
+
+    context = {
+        'project': project,
+        'task': task,
+        'entry': entry,
+        'hours': hours,
+        'minutes': minutes
+    }
+
+    return render(request, 'project/edit_entry.html', context)
+
+@login_required
+def delete_entry(request, project_id, task_id, entry_id):
+    project = get_object_or_404(Project, pk=project_id)
+    task = get_object_or_404(Task, pk=task_id)
+    entry = get_object_or_404(Entry, pk=entry_id)
+    entry.delete()
+
+    messages.info(request, 'The entry was deleted!')
+
+    return redirect('project:task', project_id=project.id, task_id=task.id) 
